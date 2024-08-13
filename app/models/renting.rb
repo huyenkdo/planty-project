7b6ba383792084f4f49f5b80f28e7c0828c7ef35
Huyen Do <huyenkdo@gmail.com>
2024-08-13 11:47:04 +0200

    added assocaitions and validations for all models

--- app/models/renting.rb
+++ app/models/renting.rb
@@ -1,4 +1,19 @@
 class Renting < ApplicationRecord
   belongs_to :plant
   belongs_to :user
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :start_date_cannot_be_in_the_past
+  validate :end_date_cannot_be_before_start_date
+
+  def start_date_cannot_be_in_the_past
+    errors.add(:start_date, "ne peut pas être dans le passé") if
+      !start_date.blank? and start_date < Date.today
+  end
+
+  def end_date_cannot_be_before_start_date
+    errors.add(:end_date, "ne peut pas être avant la date de début de location") if
+      end_date < start_date
+  end
 end
