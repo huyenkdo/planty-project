4f233ca1dfa73500c4e19c4bc5b63476622d90dd
Huyen Do <huyenkdo@gmail.com>
2024-08-13 17:02:04 +0200

    coded back dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,12 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: :dashboard
   def home
   end
 
   def dashboard
+    @user = current_user
+    @own_plants = @user.plants
+    @rentings = @user.rentings
   end
 
   def accept
