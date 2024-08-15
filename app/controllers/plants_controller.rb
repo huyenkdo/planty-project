5065340dd8f882af037fd45624521e18b79a52b0
timothee keck <timotheek92160@gmail.com>
2024-08-15 11:12:44 +0200

    searchbar fonctionnel

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,6 +1,10 @@
 class PlantsController < ApplicationController
   def index
-    @plants = Plant.all
+    if params[:query].present?
+      @plants = Plant.search_by_name_category_description(params[:query])
+    else
+      @plants = Plant.all
+    end
   end
 
   def show
