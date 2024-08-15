2c9f60d2d34cd553d27f62145cacc564ef674c44
meryem hassaine <hass.meryem@gmail.com>
2024-08-15 16:05:45 +0200

    edit fini normalement qui renvoi au show

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2024_08_15_082957) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_15_105444) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +40,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_15_082957) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "flats", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pg_search_documents", force: :cascade do |t|
