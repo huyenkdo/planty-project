22b1e58599454deea994c8ed7c6cb2ea55dc9cc0
meryem hassaine <hass.meryem@gmail.com>
2024-08-15 17:27:26 +0200

    alerte sur page show pour reservation faite

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @plants = Plant.all
     if params[:filter].present?
--- app/controllers/rentings_controller.rb
+++ app/controllers/rentings_controller.rb
@@ -1,4 +1,20 @@
 class RentingsController < ApplicationController
   def create
+    @renting = Renting.new(renting_params)
+    @renting.user = current_user
+    plant = Plant.find(params[:plant_id])
+    @renting.plant = plant
+
+    if @renting.save
+      redirect_to plant_path(plant), notice: "Votre demande de réservation a été envoyée avec succès."
+    else
+      render :new, alert: "Il y a eu un problème avec votre demande de réservation."
+    end
+  end
+
+  private
+
+  def renting_params
+    params.require(:renting).permit(:start_date, :end_date)
   end
 end
